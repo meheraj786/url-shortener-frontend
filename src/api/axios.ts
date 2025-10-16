2bf062e396b5ecea1ace76dccae9d95a9c4d31d8
meheraj786 <meherajhosen786@gmail.com>
2025-10-16 23:45:12 +0600

    axios update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://url-shortener-backend-ngiy.onrender.com/api/v1",
+  baseURL: "https://yourl-f9d4.onrender.com/api/v1",
   withCredentials: true, 
 });
 
