cc72bc31fa99bd26421d7c3daa9b5c709596efae
meheraj786 <meherajhosen786@gmail.com>
2025-10-17 20:26:01 +0600

    bug fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://yourl-f9d4.onrender.com/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
   withCredentials: true, 
 });
 
